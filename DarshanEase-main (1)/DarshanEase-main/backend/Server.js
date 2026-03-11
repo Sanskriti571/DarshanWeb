6ed9acbb78b498ab6b51802356b52375074beebe
Sanskriti571 <asanskriti93@gmail.com>
2026-03-11 23:38:34 +0530

    Add files via upload

---
+++ DarshanEase-main (1)/DarshanEase-main/backend/Server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+require('./config/db');
+const cors = require('cors');
+
+const userRoutes = require('./routes/userRoutes');
+const organizerRoutes = require('./routes/organizerRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://darshanease.onrender.com", "https://darshanease-frontend.onrender.com", "https://darshanease-frontend-lc1m.onrender.com"],
+  methods: ["POST", "GET", "DELETE", "PUT"],
+  credentials: true
+}));
+
+// Routes
+app.use('/user', userRoutes);
+app.use('/organizer', organizerRoutes);
+app.use('/admin', adminRoutes);
+
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+  console.log(`Port is listening at ${PORT}`);
+});
